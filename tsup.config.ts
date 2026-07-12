69616c7ba9d86bd6662d04f2217df972a1f55c36
adnan6688 <adnanfarukgolam680@gmail.com>
2026-07-12 11:11:17 +0600

    fix: final tsconfig fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,54 +4,37 @@ import { defineConfig } from "tsup";
 
 
 
-// export default defineConfig({
-
-//     entry: ["src/server.ts"],
-
-//     format: ["esm"], // Keep this as ESM
-
-//     target: "esnext",
-
-//     outDir: "dist",
-
-//     clean: true,
-
-//     bundle: true,
-
-//     splitting: false,
-
-//     sourcemap: true,
-
-//     // Add this banner to shim require() for CJS dependencies
-
-//     banner: {
-
-//         js: `
-
-//    import { createRequire } from 'module';
-
-//    const require = createRequire(import.meta.url);
-
-//   `,
-
-//     },
-
-// });
-
-
 export default defineConfig({
+
     entry: ["src/server.ts"],
-    format: ["esm"], 
+
+    format: ["esm"], // Keep this as ESM
+
     target: "esnext",
-    outDir: "api", // 'dist' থেকে বদলে 'api' করুন
+
+    outDir: "dist",
+
     clean: true,
+
     bundle: true,
+
     splitting: false,
+
     sourcemap: true,
+
+    // Add this banner to shim require() for CJS dependencies
+
     banner: {
+
         js: `
+
    import { createRequire } from 'module';
+
    const require = createRequire(import.meta.url);
+
   `,
+
     },
+
 });
+
